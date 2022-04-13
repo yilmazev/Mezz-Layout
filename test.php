fd1d1eb86bfd7d0ca4ef8c73a0996aac8abcb7cc
Yılmaz Ev <81462055+yilmazev@users.noreply.github.com>
2022-04-13 17:37:02 +0300

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<style>
-.test {
-background-repeat:no-repeat;background-position:center top;width:288px;height:284px;text-align:center;margin-left:-7px;padding:20px 7px 0 0;position:relative}	
-}
-.test:before{background-image:url(https://fresh-hotel.org/assets/images/web/pages/registration/clouds.png);background-repeat:no-repeat;width:100%;height:100%;position:absolute;top:0;left:0;z-index:2;display:block;content:""}
-</style>
-<body>
-<div class="test"></div>
-</body>
