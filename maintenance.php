9756163cf3f7e65b70e129aa2347641ed569e578
Yılmaz Ev <81462055+yilmazev@users.noreply.github.com>
2022-04-19 18:30:13 +0300

    Maintenance page added

---
+++ maintenance.php
@@ -0,0 +1,71 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="/assets/styles/app.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <title>Virtual World, Avatar Chat and Pixel Art - Habbo</title>
+</head>
+<body class="container">
+	<script src="/assets/scripts/page-load.js"></script>
+    <div class="page-content">
+        <header class="page-content-header pixelated">
+            <div class="page-content-max-width">
+                <div class="page-content-header-column">
+                    <img src="https://habbofont.net/font/habbo_new_big/habbo.gif" alt="Habbo Big Logo" class="page-content-header-logo">
+                </div>
+            </div> 
+        </header>
+        <div class="page-content-collider" style="background-color: transparent;">
+            <div class="page-content-max-width" style="flex-direction: column;align-items: flex-start;">
+                <div class="page-content-collider-item">
+                    <div class="page-content-collider-content maintenance">
+                        <div class="page-content-collider-content-maintenance-box">
+                            <h2 class="page-content-collider-content-maintenance-box-title">Maintenance Break</h2>
+                            <p class="page-content-collider-content-maintenance-box-text">Sorry! Habbo is being worked on al. Use moment. We'll be back soon. We promise.</p>
+                            <img src="/assets/images/maintenance/frank.png" alt="Maintenance" class="page-content-collide-item-content-maintenance-box-image">
+                        </div>
+                        <iframe src="https://discord.com/widget?id=952572046196494426&theme=light" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="page-content-footer">
+            <div class="page-content-footer-top-side">
+                <div class="page-content-max-width space-between" style="align-items: flex-start;">
+                    <div class="page-content-footer-column">
+                        <img class="page-content-footer-logo" src="https://habbofont.net/font/habbo_big/habbo.gif">
+                        <p class="page-content-footer-text">Habbo is an online vintage pixel-art style virtual community where you can create your own avatar, make friends, chat, build rooms, design + play games and so much more! Almost anything is possible in this strange place full of awesome people…</p>
+                    </div>
+                    <div class="page-content-footer-column">
+                        <h3 class="page-content-footer-title">Support</h3>
+                        <a href="" class="page-content-footer-url">Help Center</a>
+                        <a href="" class="page-content-footer-url">Recover Password</a>
+                        <a href="mailto:ylmzofc@outlook.com" class="page-content-footer-url">ylmzofc@outlook.com</a>
+                    </div>
+                    <div class="page-content-footer-column">
+                        <h3 class="page-content-footer-title">Security and Privacy</h3>
+                        <a href="/playing-habbo/safety.php" class="page-content-footer-url">Safety</a>
+                        <a href="" class="page-content-footer-url">Terms of Service</a>
+                        <a href="" class="page-content-footer-url">Privacy Policy</a>
+                        <a href="" class="page-content-footer-url">Cookie Policy</a>
+                    </div>
+                    <div class="page-content-footer-column">
+                        <h3 class="page-content-footer-title">Desktop App</h3>
+                        <p class="page-content-footer-text">Download the desktop app for easier access to the hotel.</p>
+                        <a href="" class="page-content-footer-download-button">Download HabboApp</a>
+                    </div>
+                </div>
+            </div>
+            <div class="page-content-footer-bottom-side">
+                <div class="page-content-max-width space-between" style="align-items: flex-start;">
+                    <span class="page-content-footer-title copyright">&copy; <script>var date=new Date();var year=date.getFullYear();document.write(year);</script> Habbo Hotel.</span>
+                    <p class="page-content-footer-text copyright">All rights reserved. Design and coding by <a href="https://yilmazev.github.io" target="_blank" class="page-content-footer-url hugoyin">Hugoyin</a>.</p>
+                </div>
+            </div>
+        </footer>
+		<script src="/assets/scripts/app.js"></script>
+    </div>
+</body>
+</html>
